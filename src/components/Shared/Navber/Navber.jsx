3696c52d14a0400992129b7e3911b442a1bce43f
AB-ohi <askohi63907@gmail.com>
2023-05-02 16:15:47 +0600

    first commit

---
+++ src/components/Shared/Navber/Navber.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Navber.css'
+const Navber = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Eatery <samp className='name'>.</samp> Cafe</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="mx-auto my-2 my-lg-0 nav-link"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Link to="/">Home</Link>
+            <Link to="/blog">Blog</Link>
+          </Nav>
+            <Link to='/login'><Button variant="danger">Login</Button></Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        </div>
+    );
+};
+
+export default Navber;
