a1275125440eb496bacb545cf3cde339b3c7afd0
AB-ohi <askohi63907@gmail.com>
2023-05-02 16:19:51 +0600

    add fire base

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgqBwXEivBPCJUXD5fcTGF4aFIVRGdxpQ",
+  authDomain: "restaurants-project-2041e.firebaseapp.com",
+  projectId: "restaurants-project-2041e",
+  storageBucket: "restaurants-project-2041e.appspot.com",
+  messagingSenderId: "338700911800",
+  appId: "1:338700911800:web:7f7844c786b121b113204d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
