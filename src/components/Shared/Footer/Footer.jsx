ca67e57ee3310ca3b327244b742c11721c6dcc04
AB-ohi <askohi63907@gmail.com>
2023-05-03 21:48:37 +0600

    footer add leftside

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Footer.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faFacebook } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <div>1</div>
-            <div>2</div>
+            <div className='left-side'>
+                <h1>Follow</h1>
+                <div>
+                {/* <FontAwesomeIcon icon={faFacebook} /> */}
+                </div>
+                <h1>Address</h1>
+                <p>1234 Street Name</p>
+                <p>City, AA 99999</p>
+
+                <h1>Contacts</h1>
+                <p>Email: support@mobirise.com</p>
+                <p>Phone: +1 (0) 000 0000 001</p>
+                <p>Fax: +1 (0) 000 0000 002</p>
+            </div>
+            <div>
+                <h1>Opening Hours</h1>
+                
+            </div>
             <div>3</div>
         </div>
     );
